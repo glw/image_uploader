0e295039f15ec6915aa1922aed33bed930bf52e6
glw <garretlewis@gmail.com>
2018-08-06 12:06:26 -0500

    updated upload code, uploads to postgresql but still working on fixing issue when uploading image to spaces on digitalocean.

--- image_upload.py
+++ image_upload.py
@@ -1,16 +1,32 @@
-# FPDCC image uploader for signs and utilities.
+# FPDCC image uploader for signs, utilities, benches, and other "stuff".
+# Basic usage:
+# python image_upload.py -i ../signs_to_be_uploaded/image.jpg
+
 import yaml
 import boto3
 import uuid
-#import PIL
-#import csvkit
 import argparse
 import os
 import exifread
 import psycopg2
 import psycopg2.extras
-#from exif_read import ExifRead
-#from geo import normalize_bearing, interpolate_lat_lon, gps_distance
+
+#############
+## Env setup
+#############
+config = yaml.safe_load(open("config.yaml"))
+# postgresql db
+HOST = config['postgresql']['HOST']
+USER = config['postgresql']['USER']
+PASSWD = config['postgresql']['PASSWD']
+DATABASE = config['postgresql']['DATABASE']
+# D.O. spaces
+REGION_NAME = config['spaces']['REGION_NAME']
+ENDPOINT_URL = config['spaces']['ENDPOINT_URL']
+ACCESS_KEY_ID = config['spaces']['ACCESS_KEY_ID']
+SECRET_ACCESS_KEY = config['spaces']['SECRET_ACCESS_KEY']
+BUCKETNAME = config['spaces']['BUCKETNAME']
+SPACESPATH = config['spaces']['SPACESPATH']
 
 ###############
 ## Parse inputs
@@ -25,10 +41,21 @@ args = vars(ap.parse_args())
 input_filename = args["input"]
 file_path, file_basename = os.path.split(input_filename)
 # rename file
-#new_name = uuid.uuid4().hex
-#output_jpg = new_name + '.jpg'
-output_jpg = file_basename
-#os.rename(os.path.join(file_path,file_basename), os.path.join(file_path,output_jpg))
+# new_name = uuid.uuid4().hex
+output_jpg = uuid.uuid4().hex + '.jpg'
+# output_jpg = file_basename
+os.rename(os.path.join(file_path,file_basename), os.path.join(file_path,output_jpg))
+
+print('##################')
+print('Processing File: {} ').format(input_filename)
+print('##################')
+print('Renaming: {} to {} ').format(file_basename, output_jpg)
+print('##################')
+
+
+## TO DO:
+## 1. resize and minimize files
+## 2. tensorflow image classification for automated sign types.
 
 
 # source: https://github.com/ianare/exif-py/issues/66
@@ -67,7 +94,7 @@ def get_coordinates(tags):
 class DBException(Exception):
     pass
 class DB():
-    def __init__(self, parent_widget):
+    def __init__(self):
         self.conn = None
         self.dbhost = HOST
         self.dbname = DATABASE
@@ -79,16 +106,16 @@ class DB():
         Connects to the database
         :return:
         """
-    if self.conn is None:
-        try:
-            self.conn = psycopg2.connect(
-                host=self.dbhost,
-                database=self.dbname,
-                user=self.dbuser,
-                password=self.dbpass
-                )
-        except psycopg2.OperationalError as e:
-            raise DBException("Error connecting to database on '%s'. %s" % (self.dbhost, str(e)))
+        if self.conn is None:
+            try:
+                self.conn = psycopg2.connect(
+                    host=self.dbhost,
+                    database=self.dbname,
+                    user=self.dbuser,
+                    password=self.dbpass
+                    )
+            except psycopg2.OperationalError as e:
+                raise DBException("Error connecting to database on '%s'. %s" % (self.dbhost, str(e)))
 
     def _close(self):
         """
@@ -99,17 +126,17 @@ class DB():
             self.conn.close()
             self.conn = None
 
-    def insert_data(self, sql):
+    def insert_data(self, sql, data):
         """
         :param sql: text string with the sql statement
         :return:
         """
         self._connect()
         cur = self.conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        #print(sql)
-        cur.execute(sql)
+        cur.execute(sql, data)
         self.conn.commit()
         self._close()
+DB = DB()
 
 with open(os.path.join(file_path,output_jpg), 'rb') as f:
     try:
@@ -118,24 +145,35 @@ with open(os.path.join(file_path,output_jpg), 'rb') as f:
         ###############
         tags = exifread.process_file(f)
         coordinates = get_coordinates(tags)
-        latitude = coordinates[1]
-        longitude =coordinates[0]
+        latitude = str(coordinates[1])
+        longitude = str(coordinates[0])
 
 
         ##############################
         ## Upload metadata to postgis
         ##############################
-
         path = SPACESPATH
-        fullpath = SPACESPATH + '/' + output_jpg
-        sql = 'insert into public.signage (name, path, fullpath, latitude, longitude, geom) values(%s, %s, %s, %s, %s, ST_MakePoint(%s, %s), 4326));', (output_jpg, path, fullpath, latitude, longitude, longitude, latitude)
-        DB = DB()
-        DB.insert_data(sql)
+        full_path = path + output_jpg
 
+        sql = 'insert into public.signage (name, path, full_path, latitude, longitude) values(%s, %s, %s, %s, %s);'
 
+        data = (output_jpg, path, full_path, latitude, longitude)
 
+        #print(sql)
+        #print(data)
+        print('Creating point at: Latitude: {}, Longitude: {} ').format(latitude, longitude)
+        print('###################')
+
+        DB.insert_data(sql, data)
+    except:
+        Print('could not insert data')
+
+    finally:
+        f.close()
+
+    try:
         ######################################
-        ## Upload file to Digital Ocean Spaces
+        print('Upload file to Digital Ocean Spaces')
         ######################################
         # s3cmd put file.jpg --acl-public s3://spacename/path/
         # s3cmd setacl s3://spacename/file.jpg --acl-public
@@ -145,15 +183,20 @@ with open(os.path.join(file_path,output_jpg), 'rb') as f:
         client = session.client('s3',
                                 region_name=REGION_NAME,
                                 endpoint_url=ENDPOINT_URL,
-                                aws_access_key_id=AWS_ACCESS_KEY_ID,
-                                aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+                                aws_access_key_id=ACCESS_KEY_ID,
+                                aws_secret_access_key=SECRET_ACCESS_KEY)
 
         # upload file
-        client.upload_file('/path/to/file.jpg',  # Path to local file
+        client.upload_file(os.path.join(file_path,output_jpg),  # Path to local file
                             BUCKETNAME,  # Name of Space
-                            'remote_file.jpg')  # Name for remote file
+                            output_jpg)  # Name for remote file
         # make file public after upload
         client.put_object_acl( ACL='public-read', Bucket=BUCKETNAME, Key=KEYNAME )
 
-    finally:
-        f.close()
+        print('##################')
+        print('{} uploading complete. ').format(output_jpg)
+        print('#################')
+
+    except Exception as e:
+        print('Error: Could not uploade file.')
+        print(e)
